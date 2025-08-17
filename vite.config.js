0de9199b93c1aad8879584fb08f43ed3b231c81e
zekie1604 <keanlucas54926@gmail.com>
2025-08-17 15:16:17 +0800

    a

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-router', 'react-router-dom']
-  },
   build: {
-    rollupOptions: {
-      external: [],
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router', 'react-router-dom']
-        }
-      }
-    },
     chunkSizeWarningLimit: 1000
   }
 })
