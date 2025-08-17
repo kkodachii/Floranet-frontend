2b890cd390192ca3e1bae1353d04b5713d3d9470
zekie1604 <keanlucas54926@gmail.com>
2025-08-17 15:12:12 +0800

    vercel

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react-router', 'react-router-dom']
+  },
   build: {
     rollupOptions: {
-      external: []
-    }
+      external: [],
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router', 'react-router-dom']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
   }
 })
