c31991d1bdcfa682cae580c7358f65b970bec7c0
zekie1604 <keanlucas54926@gmail.com>
2025-08-17 15:58:01 +0800

    Fix Vercel deployment: Update react-router versions and vite config

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router', 'react-router-dom']
-        }
-      }
-    }
-  }
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  optimizeDeps: {
+    include: ['react-router-dom'],
+    exclude: ['@remix-run/router'],
+  },
 })
