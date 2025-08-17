bd7dbe624ecfe707dd653dfff01ab13c381703d0
kkodachii <keanlucas54926@gmail.com>
2025-08-17 14:51:36 +0800

    a

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-router': 'react-router/dist/index.js',
-      'react-router-dom': 'react-router-dom/dist/index.js'
-    }
-  },
   optimizeDeps: {
     include: ['react-router', 'react-router-dom']
   }
