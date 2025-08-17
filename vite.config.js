e601853da60e422495fa480920a4aa7e7172a7b7
kkodachii <keanlucas54926@gmail.com>
2025-08-17 14:42:22 +0800

    asd

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router', 'react-router-dom']
-        }
-      }
+    commonjsOptions: {
+      include: [/node_modules/]
     }
   }
 })
