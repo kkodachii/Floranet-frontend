c9f83774ac86c32fa0c34154f0710d6db7eebec5
kkodachii <keanlucas54926@gmail.com>
2025-08-17 14:50:38 +0800

    v

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'react-router': 'react-router/dist/main.js',
-      'react-router-dom': 'react-router-dom/dist/main.js'
+      'react-router': 'react-router/dist/index.js',
+      'react-router-dom': 'react-router-dom/dist/index.js'
     }
   },
   optimizeDeps: {
